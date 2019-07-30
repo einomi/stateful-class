ef0b7189d344850a19e2f8a1ff5e00ef56bd8c76
Andrey Nikonov <a.einomi@gmail.com>
2019-07-30 21:21:30 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+export function Stateful(props) {
+  this.props = props;
+  this.onInit();
+  if (typeof module !== 'undefined' &&  module.hot) {
+    module.hot.dispose(() => {
+      this.onDestroy();
+    });
+  }
+}
+
+Stateful.prototype.setState = function(stateToMerge) {
+  const prevState = {
+    ...this.state,
+  };
+  this.state = {
+    ...this.state,
+    ...stateToMerge,
+  };
+  this.onStateUpdate(prevState);
+};
+
+export default Stateful
